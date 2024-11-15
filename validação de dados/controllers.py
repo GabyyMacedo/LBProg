a3e6c6184643fae0565e99de7f43ede614c5cc17
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2024-11-14 21:02:47 -0300

    Delete validação de dados directory

--- validação de dados/controllers.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template, request
-
-dados_controllers=Blueprint('dado', __name__)
-
-@dados_controllers.route("/", methods=["GET", "POST"])
-def index():
-    return render_template('index.html')
