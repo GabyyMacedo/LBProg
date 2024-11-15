88ca9af8d716f0dd65b0cf6388f96bf6920bcf16
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2024-11-14 21:02:39 -0300

    Delete flask directory

--- flask/recomposição/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from controllers import alunos_controllers
-app = Flask(__name__)
-app.register_blueprint(alunos_controllers)
-if __name__=="__main__":
-    app.run(debug=True)
