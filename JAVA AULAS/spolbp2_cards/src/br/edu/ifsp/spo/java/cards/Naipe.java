fcdddf0379a86cf167ce3ea0003f6951f11b3413
Gaby Macedo <135060010+GabyyMacedo@users.noreply.github.com>
2025-04-23 11:27:33 -0300

    Delete JAVA AULAS/spolbp2_cards/src/br/edu/ifsp/spo/java/cards directory

--- JAVA AULAS/spolbp2_cards/src/br/edu/ifsp/spo/java/cards/Naipe.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifsp.spo.java.cards;
-
-public enum Naipe {
-    COPAS, OUROS, PAUS, ESPADAS;
-}
--- JAVA AULAS/spolbp2_cards/src/br/edu/ifsp/spo/java/cards/Valor.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifsp.spo.java.cards;
-
-public enum Valor {
-    AS, DOIS, TRES, QUATRO, CINCO, SEIS, SETE, OITO, NOVE, DEZ, DAMA, VALETE, REI;
-}
